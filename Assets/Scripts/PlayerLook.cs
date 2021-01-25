a4ba81c235ec9d476a5f1fbedeb8f1c51e7bfedb
Plai-Dev <xdplai1@gmail.com>
2021-01-24 21:06:23 -0500

    02 Jumping and Looking

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField] private float sensX;
+    [SerializeField] private float sensY;
+
+    Camera cam;
+
+    float mouseX;
+    float mouseY;
+
+    float multiplier = 0.01f;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        // get component in child *obvious
+        cam = GetComponentInChildren<Camera>();
+
+        // lock cursor and make invisible
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        // read first
+        MyInput();
+
+        // local rotation as camera is a child of player and we only want to look up and down with the camera so pass in xRotation
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        // only want to look left and right so pass in yRotation
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+    void MyInput()
+    {
+        // get mouse input (already framrate independent)
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        // multiplier to reduce sensitivity
+        yRotation += mouseX * sensX * multiplier;       //  add to yRotation as we want to rotate on the Y axis to look horizontally
+        xRotation -= mouseY * sensY * multiplier;       //  subtracting, otherwise it is inverted
+
+        // clamping rotation 
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    float playerHeight = 2f;
+
     [Header("Movement")]
     public float moveSpeed = 6f;
-    
+    [SerializeField] float airMultiplier = 0.4f;
     float movementMultiplier = 10f;
-    float rbDrag = 6f;
+
+    [Header("Jumping")]
+    public float jumpForce = 5f;
+
+    [Header("Keybinds")]
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Drag")]
+    [SerializeField] float groundDrag = 6f;
+    [SerializeField] float airDrag = 2f;
+
 
     float horizontalMovement;
     float verticalMovement;
+
+    bool isGrounded;
 
     Vector3 moveDirection;
 
@@ -23,11 +37,17 @@ public class PlayerMovement : MonoBehaviour
         rb.freezeRotation = true;
     }
 
-
     private void Update()
     {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight / 2 + 0.1f);
+
         MyInput();
         ControlDrag();
+
+        if (Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            Jump();
+        }
     }
 
     void MyInput()
@@ -38,9 +58,21 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
     }
 
+    void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
     void ControlDrag()
     {
-        rb.drag = rbDrag;
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = airDrag;
+        }
     }
 
     private void FixedUpdate()
@@ -50,6 +82,13 @@ public class PlayerMovement : MonoBehaviour
 
     void MovePlayer()
     {
-        rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        if (isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        }
+        else if (!isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier * airMultiplier, ForceMode.Acceleration);
+        }
     }
 }
